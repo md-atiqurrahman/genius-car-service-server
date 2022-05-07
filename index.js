ee00615f65a40b5083af477ffb4df7e18370570d
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-05-07 15:51:17 +0600

    add steps

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('geniusDB').collection('service');
+        const orderCollection = client.db('geniusDB').collection('order');
 
         app.get('/service', async (req , res) =>{
             const query = {};
@@ -42,6 +43,21 @@ async function run(){
             const result = await serviceCollection.insertOne(newService);
             res.send(result);
         });
+
+        // post method for order
+        app.get('/order', async(req, res) =>{
+            const email = req.query.email;
+            const query = {email: email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders)
+        })
+
+        app.post('/order', async (req , res) =>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
 
         app.delete('/service/:id', async (req ,res) =>{
             const id  = req.params.id;
